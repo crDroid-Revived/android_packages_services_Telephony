fb0a1fc48243baaaee11663dcd9da94cb2051862
Thomas Stuart <tjstuart@google.com>
2025-04-29 21:12:44 +0000

    Remove the contacts picker from VoicemailSettingsActivity
    For older versions of Android Tiramisu (13) and
    UpsideDownCake (14), the contacts picker should be
    removed since there is no good API for verifying
    the calling process.
    
    Flag: EXEMPT security fix
    Bug: 337785563
    Test: manual (A) contact picker is removed for voicemail activity
                 (B) contacts picker is still present on forwarding
    	         activity
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:7a4dd0f68c6fd09f493820ab9a96902f64fd1b38)
    Merged-In: I7510c167e3f063ac48840bc9ccab66ab26d046ce
    Change-Id: I7510c167e3f063ac48840bc9ccab66ab26d046ce

--- src/com/android/phone/EditPhoneNumberPreference.java
+++ src/com/android/phone/EditPhoneNumberPreference.java
@@ -136,6 +136,8 @@ public class EditPhoneNumberPreference extends EditTextPreference
     // button that was clicked on dialog close.
     private int mButtonClicked;
 
+    private boolean mContactPickerEnabled = true;
+
     /**
      * Interface for the dialog closed listener, related to
      * DialogPreference.onDialogClosed(), except we also pass in a buttonClicked
@@ -191,6 +193,13 @@ public class EditPhoneNumberPreference extends EditTextPreference
         this(context, null);
     }
 
+    /**
+     * Sets whether the contact picker button should be enabled and visible.
+     * @param enabled true to enable the contact picker, false to disable and hide it.
+     */
+    public void setContactPickerEnabled(boolean enabled) {
+        mContactPickerEnabled = enabled;
+    }
 
     /*
      * Methods called on UI bindings
@@ -262,13 +271,20 @@ public class EditPhoneNumberPreference extends EditTextPreference
 
         //set contact picker
         if (mContactPickButton != null) {
-            mContactPickButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    if (mParentActivity != null) {
-                        mParentActivity.startActivityForResult(mContactListIntent, mPrefId);
+            if (mContactPickerEnabled) {
+                mContactPickButton.setVisibility(View.VISIBLE);
+                mContactPickButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        if (mParentActivity != null && mContactListIntent != null) {
+                            mParentActivity.startActivityForResult(mContactListIntent, mPrefId);
+                        }
                     }
-                }
-            });
+
+                });
+            } else {
+                mContactPickButton.setVisibility(View.GONE);
+                mContactPickButton.setOnClickListener(null);
+            }
         }
         //set timer settings
         initTimeSettingsView(view);
